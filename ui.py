8ddf6b0bba7a7b7cd3c8f78704440561a23e5f94
phange <gerry.phan@gmail.com>
2022-08-08 21:46:08 -0700

    removed smelly code

--- ui.py
+++ ui.py
@@ -8,20 +8,14 @@ while True:
     time.sleep(1.0)
     f = open("signal.txt", "r")
     result = f.readline()
-    # result = list(result)
-    # print(type(result))
     if result.count("id")> 1 :
         count = result.count("id")
         count = ("The amount of tasks you currently have: " + str(count))
         print(count)
-    
-    # json.loads(result)
-    
+        
     if len(result) > 1 :#"run\n"
         f.close()
         f = open('signal.txt', "w")
         f.write(count)
-        # print("Writing number of tasks")
-        
-        
+    
     f.close()
